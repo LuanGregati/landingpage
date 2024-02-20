52214715e06c514edc25568220d51128d57a11ad
Luan Gregati <luan.luiz.msn@hotmail.com>
2024-02-20 13:42:44 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+    // documentElement é a tag HTML
+    const html = document.documentElement;
+
+    html.classList.toggle('light'); // Utilizo uma função interna que adiciona a classe se não tiver, remove se tiver.
+
+    // Pego a tag da imagem usando query selector
+    const img = document.querySelector('#profile img');
+
+    // Verifico se na lista de classes atribuídas ao elemento, eu possuo a classe "light"
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/luan-light.jpg');
+    }else{
+        img.setAttribute('src', './assets/luan-dark.jpg');
+    }
+}
